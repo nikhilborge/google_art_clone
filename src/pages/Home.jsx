4221d4812b4a00d80023ef486836d535ec441373
nikhil borge <nikhilb3439.nb@gmail.com>
2023-07-01 16:58:38 +0530

    navbar and home page section1 added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Section1 from "../components/sections/Section1";
+
+const Home = () => {
+  return (
+    <>
+      <Section1/>
+    </>
+  );
+};
+
+export default Home;
